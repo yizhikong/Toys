6d0c8371f62a257aa926ee7b849a5bd1cc0b1e4f
yizhikong <343802694@qq.com>
2014-07-14 11:15:47 +0800

    python sendEmail

--- sendEmail.py
+++ sendEmail.py
@@ -2,18 +2,18 @@
 import smtplib
 from email.mime.text import MIMEText
 def sendEmail(fromUser, password, toUser, title, content):
-    parse = toUser.split('@')
-    mailHost = 'smtp.' + parse[1]
-    #mailPostFix = parse[1]
-    msg = MIMEText(content, _subtype = 'plain', _charset = 'gb2312')
-    msg['Subject'] = title
-    msg['From'] = fromUser
-    msg['to'] = toUser
-    server = smtplib.SMTP()
-    server.connect(mailHost)
-    server.login(fromUser, password)
-    #print mailHost
-    server.sendmail(formUser, toUser, msg.as_string())
-    server.close()
+	parse = fromUser.split('@')
+	mailHost = 'smtp.' + parse[1]
+	#mailPostFix = parse[1]
+	msg = MIMEText(content, _subtype = 'plain', _charset = 'gb2312')
+	msg['Subject'] = title
+	msg['From'] = fromUser
+	msg['to'] = toUser
+	server = smtplib.SMTP()
+	server.connect(mailHost)
+	server.login(fromUser, password)
+	#print mailHost
+	server.sendmail(fromUser, toUser, msg.as_string())
+	server.close()
 
-sendEmail('xxxxx@qq.com', 'mypassword', 'xxxxx@qq.com', 'My first eamil', 'Hello world!')
+sendEmail('xxx@163.com', 'yourpassword', 'xxx@xxx.com', 'My first eamil', 'Hello world!')
