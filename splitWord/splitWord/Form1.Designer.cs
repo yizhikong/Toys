f291dc4ba4083ded02b44571efd6d5dfd3416aab
yizhikong <343802694@qq.com>
2015-08-30 23:46:33 +0800

    C sharp

---
+++ splitWord/splitWord/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace splitWord
+{
+    partial class CSpliter
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tx_insert = new System.Windows.Forms.TextBox();
+            this.tx_sentence = new System.Windows.Forms.TextBox();
+            this.btn_insert = new System.Windows.Forms.Button();
+            this.btn_split = new System.Windows.Forms.Button();
+            this.tx_answer = new System.Windows.Forms.Label();
+            this.tx_label = new System.Windows.Forms.Label();
+            this.tx_search = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tx_insert
+            // 
+            this.tx_insert.Location = new System.Drawing.Point(28, 44);
+            this.tx_insert.Name = "tx_insert";
+            this.tx_insert.Size = new System.Drawing.Size(126, 21);
+            this.tx_insert.TabIndex = 0;
+            // 
+            // tx_sentence
+            // 
+            this.tx_sentence.Location = new System.Drawing.Point(28, 107);
+            this.tx_sentence.Name = "tx_sentence";
+            this.tx_sentence.Size = new System.Drawing.Size(126, 21);
+            this.tx_sentence.TabIndex = 1;
+            // 
+            // btn_insert
+            // 
+            this.btn_insert.Location = new System.Drawing.Point(170, 41);
+            this.btn_insert.Name = "btn_insert";
+            this.btn_insert.Size = new System.Drawing.Size(75, 23);
+            this.btn_insert.TabIndex = 2;
+            this.btn_insert.Text = "添加词汇";
+            this.btn_insert.UseVisualStyleBackColor = true;
+            this.btn_insert.Click += new System.EventHandler(this.btn_insert_Click);
+            // 
+            // btn_split
+            // 
+            this.btn_split.Location = new System.Drawing.Point(170, 107);
+            this.btn_split.Name = "btn_split";
+            this.btn_split.Size = new System.Drawing.Size(75, 23);
+            this.btn_split.TabIndex = 3;
+            this.btn_split.Text = "进行分词";
+            this.btn_split.UseVisualStyleBackColor = true;
+            this.btn_split.Click += new System.EventHandler(this.btn_split_Click);
+            // 
+            // tx_answer
+            // 
+            this.tx_answer.AutoSize = true;
+            this.tx_answer.Location = new System.Drawing.Point(26, 180);
+            this.tx_answer.Name = "tx_answer";
+            this.tx_answer.Size = new System.Drawing.Size(0, 12);
+            this.tx_answer.TabIndex = 4;
+            // 
+            // tx_label
+            // 
+            this.tx_label.AutoSize = true;
+            this.tx_label.Location = new System.Drawing.Point(28, 157);
+            this.tx_label.Name = "tx_label";
+            this.tx_label.Size = new System.Drawing.Size(29, 12);
+            this.tx_label.TabIndex = 5;
+            this.tx_label.Text = "结果";
+            // 
+            // tx_search
+            // 
+            this.tx_search.Location = new System.Drawing.Point(28, 75);
+            this.tx_search.Name = "tx_search";
+            this.tx_search.Size = new System.Drawing.Size(126, 21);
+            this.tx_search.TabIndex = 6;
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(170, 75);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(75, 23);
+            this.btn_search.TabIndex = 7;
+            this.btn_search.Text = "搜索词汇";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(28, 11);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(217, 23);
+            this.btn_load.TabIndex = 8;
+            this.btn_load.Text = "从文件中加载词汇";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // CSpliter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 345);
+            this.Controls.Add(this.btn_load);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.tx_search);
+            this.Controls.Add(this.tx_label);
+            this.Controls.Add(this.tx_answer);
+            this.Controls.Add(this.btn_split);
+            this.Controls.Add(this.btn_insert);
+            this.Controls.Add(this.tx_sentence);
+            this.Controls.Add(this.tx_insert);
+            this.Name = "CSpliter";
+            this.Text = "CSpliter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tx_insert;
+        private System.Windows.Forms.TextBox tx_sentence;
+        private System.Windows.Forms.Button btn_insert;
+        private System.Windows.Forms.Button btn_split;
+        private System.Windows.Forms.Label tx_answer;
+        private System.Windows.Forms.Label tx_label;
+        private System.Windows.Forms.TextBox tx_search;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.Button btn_load;
+    }
+}
+
---
+++ splitWord/splitWord/Form1.cs
@@ -0,0 +1,415 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace splitWord
+{
+    public partial class CSpliter : Form
+    {
+        private CAlphaTree tree;
+        private string m_insertTip = "插入{0}成功\n字典树里共有{1}个单词";
+        private string m_loadTip = "加载成功\n字典树里共有{0}个单词";
+        private string m_notExistTip = "字典树中不存在此单词";
+        private string m_existTip = "字典树中存在{0}\n" +
+            "以此单词为前缀的单词有{1}个";
+
+        public CSpliter()
+        {
+            InitializeComponent();
+            tree = new CAlphaTree();
+        }
+
+        private void btn_insert_Click(object sender, EventArgs e)
+        {
+            string word = tx_insert.Text.ToString();
+
+            tree.insertWord(word);
+            tx_answer.Text = string.Format(m_insertTip, word, tree.Count);
+            tx_insert.Text = "";
+        }
+
+        private void btn_split_Click(object sender, EventArgs e)
+        {
+            DateTime begin = DateTime.UtcNow;
+            string feedback = tree.splitSentence(tx_sentence.Text.ToString());
+            string costTime = (DateTime.UtcNow - begin).ToString();
+
+            tx_answer.Text = feedback + "\ncost time : " + costTime;
+            tx_search.Text = "";
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            string word = tx_search.Text.ToString();
+            int time = tree.searchWord(word);
+
+            if (time == 0)
+            {
+                tx_answer.Text = m_notExistTip;
+            }
+            else
+            {
+                tx_answer.Text = string.Format(m_existTip, word, time);
+            }
+
+            tx_search.Text = "";
+        }
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+            tree = CAlphaTree.createTreeByFile("H://code//splitWord//sentences.txt");
+            tx_answer.Text = string.Format(m_loadTip, tree.Count);
+        }
+    }
+
+    public class CNode
+    {
+        private CNode[] m_child;
+        public bool fWord { set; get; }
+        public int iTime { set; get; }
+
+        public CNode()
+        {
+            m_child = null;
+            fWord = false;
+            iTime = 0;
+        }
+
+        public static int getIndex(char ch)
+        {
+            int idx = Char.IsDigit(ch) ? ch - '0' + 26 : ch - 'a';
+
+            return idx;
+        }
+
+        public CNode this[int idx]
+        {
+            set
+            {
+                if (m_child == null)
+                {
+                    // how many character? 26 + 10 or 26
+                    m_child = new CNode[26 + 10];
+                }
+
+                m_child[idx] = value;
+            }
+
+            get
+            {
+                if (m_child == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    return m_child[idx];
+                }
+            }
+        }
+    }
+
+    public class CAlphaTree
+    {
+        private CNode m_root;
+        private int m_depth;
+        private List<List<string>> answers;
+        public int Count { set; get; }
+
+        public CAlphaTree()
+        {
+            m_root = new CNode();
+            Count = 0;
+            m_depth = 0;
+        }
+
+        /*****************************************************************************\
+        Function Description:
+        
+        Create a alpha tree by the contain in a file. The contain in file will be split
+        to words. Single quote mark will be replaced with " i". For example
+            that's   will be replace as   that is
+        
+        Arguments:
+
+          [IN] filename - the full path of file, which contains words
+        
+        Return Value:
+        
+          return-value - an alpha tree
+        
+        History:
+        
+          Created on 08/dd/2015 by jiaweichen_yzkk
+
+        \*****************************************************************************/
+
+        public static CAlphaTree createTreeByFile(string filename)
+        {
+            if (filename == "" || filename == null)
+            {
+                return null;
+            }
+
+            CAlphaTree tree = new CAlphaTree();
+            StreamReader reader = new StreamReader(filename);
+            string txt = reader.ReadToEnd();
+            char[] spiltChars = {' ', ',', '\n', '\t', '-', '"', '(', ')',
+                                    '\'', '.', '\r', '[', ']'};
+            string[] words = txt.Replace("'s", " is").Split(spiltChars);
+
+            foreach (string word in words)
+            {
+                if (word.Length == 0)
+                {
+                    continue;
+                }
+
+                try
+                {
+                    tree.insertWord(word.ToLower());
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+            return tree;
+        }
+
+        public void insertWord(string word)
+        {
+            // outside word will change?
+            word = word.ToLower();
+
+            if (word.Length > m_depth)
+            {
+                m_depth = word.Length;
+            }
+
+            CNode currentNode = m_root;
+            int i;
+
+            for (i = 0; i < word.Length; ++i)
+            {
+                int idx = CNode.getIndex(word[i]);
+
+                if (currentNode[idx] == null)
+                {
+                    currentNode[idx] = new CNode();
+                }
+                // add prefix
+                currentNode[idx].iTime += 1;
+                currentNode = currentNode[idx];
+            }
+
+            // mark that it is a word
+            currentNode.fWord = true;
+            // add count
+            Count += 1;
+        }
+
+        public int searchWord(string word)
+        {
+            // outside word will change?
+            word = word.ToLower();
+
+            CNode currentNode = m_root;
+            int i;
+
+            for (i = 0; i < word.Length; ++i)
+            {
+                int idx = CNode.getIndex(word[i]);
+
+                if (currentNode[idx] == null)
+                {
+                    return 0;
+                }
+                currentNode = currentNode[idx];
+            }
+
+            if (currentNode.fWord == true)
+            {
+                return currentNode.iTime;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        /*****************************************************************************\
+        Function Description:
+
+        Split a sentence(without space) to severval words according
+        the words in alpha tree
+
+        Arguments:
+
+          [IN] sentence - the sentence without space(should only contain alpha/number)
+
+        Return Value:
+
+          return-value - a string which joins the words by space
+
+        History:
+
+          Created on 08/dd/2015 by jiaweichen_yzkk
+
+        \*****************************************************************************/
+
+        public string splitSentence(string sentence)
+        {
+            answers = new List<List<string>>();
+            List<string> words = new List<string>();
+
+            cutWord(words, sentence.ToLower());
+            // cutLongestWord(words, sentence.ToLower());
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (List<string> answer in answers)
+            {
+                foreach (string word in answer)
+                {
+                    result.Append(word + " ");
+                }
+                result.Append("\n");
+            }
+            answers = null;
+            return result.ToString();
+        }
+
+        /*****************************************************************************\
+        Function Description:
+
+        cut the longest word in the head of sentence. Using recursion.
+        There is a bug(will not succeed when the longest word is not the correct word)
+
+        Arguments:
+
+          [IN] words - the list contain the words which were cut away from the sentence before
+          [IN] sentence - the uncut sentence without space(should only contain alpha/number)
+
+        Return Value:
+
+          NONE
+
+        History:
+
+          Created on 08/dd/2015 by jiaweichen_yzkk
+
+        \*****************************************************************************/
+
+        private void cutLongestWord(List<string> words, string sentence)
+        {
+            int i = 0;
+
+            while (true)
+            {
+                int length = (sentence.Length - i < m_depth) ?
+                    sentence.Length - i : m_depth;
+
+                if (length == 0)
+                {
+                    break;
+                }
+
+                string fragment = sentence.Substring(i, length);
+                string maxMatchStr = getMaxMatch(fragment);
+
+                words.Add(maxMatchStr);
+                i += maxMatchStr.Length;
+            }
+            answers.Add(words);
+        }
+
+        private string getMaxMatch(string fragment)
+        {
+            List<string> words = new List<string>();
+            CNode currentNode = m_root;
+            int i;
+
+            for (i = 0; i < fragment.Length; ++i)
+            {
+                int idx = CNode.getIndex(fragment[i]);
+
+                if (currentNode[idx] == null)
+                {
+                    break;
+                }
+                currentNode = currentNode[idx];
+                if (currentNode.fWord == true)
+                {
+                    words.Add(fragment.Substring(0, i + 1));
+                }
+            }
+            return words[words.Count - 1];
+        }
+
+
+        /*****************************************************************************\
+        Function Description:
+        
+        cut the words in the head of sentence. Each possible word will be test(recursion)
+        
+        Arguments:
+        
+          [IN] words - the list contain the words which were cut away from the sentence before
+          [IN] sentence - the uncut sentence without space(should only contain alpha/number)
+        
+        Return Value:
+        
+          NONE
+        
+        History:
+        
+          Created on 08/dd/2015 by jiaweichen_yzkk
+        
+        \*****************************************************************************/
+
+        private void cutWord(List<string> words, string sentence)
+        {
+            CNode currentNode = m_root;
+            int i;
+
+            for (i = 0; i < sentence.Length; ++i)
+            {
+                int idx = CNode.getIndex(sentence[i]);
+
+                if (currentNode[idx] == null)
+                {
+                    return;
+                }
+                currentNode = currentNode[idx];
+                if (currentNode.fWord == true)
+                {
+                    List<string> tryWords = new List<string>();
+
+                    foreach (string word in words)
+                    {
+                        tryWords.Add(word);
+                    }
+                    tryWords.Add(sentence.Substring(0, i + 1));
+                    // the last word is also a word, parse to a sentence
+                    if (i == sentence.Length - 1)
+                    {
+                        answers.Add(tryWords);
+                        return;
+                    }
+                    string nextStr = sentence.Substring(i + 1,
+                        sentence.Length - i - 1);
+
+                    cutWord(tryWords, nextStr);
+                }
+            }
+        }
+    }
+}
