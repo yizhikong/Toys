8329e51f81e79faf733683b349d69e42f42b2c94
yizhikong <343802694@qq.com>
2017-04-13 01:42:59 +0800

    make it not easy to stop

--- autoStopAndPlay/autoPlayAndStop.py
+++ autoStopAndPlay/autoPlayAndStop.py
@@ -5,6 +5,11 @@ import math
 import win32api
 import win32con
 
+# a threadsold, make the program not easy to turn "looking" to "not looking"
+ENSURE_COUNT = 4
+LOOKING = 'looking'
+NOT_LOOKING = 'not looking'
+
 class sigmoid(object):
     def __init__(self, weight):
         self.weight = weight.copy()
@@ -33,43 +38,46 @@ class BPnet(object):
         finalOutput = []
         for o in self.out:
             finalOutput.append(o.output(outputOfHidden))
-        if finalOutput[1] > finalOutput[0]:
-            return 1
-        else:
-            return 0
+        return finalOutput[1] > finalOutput[0]
 
 if __name__ == "__main__":
     net = BPnet("net.txt", 26, 2)
     # open the camera
     cap = cv2.VideoCapture(0)
-    print cap.isOpened()
-    status = "looking"
+    #print cap.isOpened()
+    status = LOOKING
+    counter = 0
     while True:
         # take a photo
         result, image = cap.read()
         height, width, depth = image.shape
-        # resize it
-        image = cv2.resize(image, (32, 24))
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # gray = cv2.equalizeHist(gray)
 
         # deal with the pixel, make it as the input of BPnet
+        gray = cv2.resize(gray, (32, 24))
         pixel = []
         heigh, width = gray.shape
         gray = gray / 255.0
         for h in range(heigh):
             for w in range(width):
                 pixel.append(gray[h][w])
+        # use neural network
+        result = net.output(np.array(pixel, dtype="float"))
 
         # check the status and take action
-        if net.output(np.array(pixel, dtype="float")):
-            if status != "looking":
-                status = "looking"
+        if result:
+            if status is NOT_LOOKING:
+                status, counter = LOOKING, 0
                 # press space to continue playing
                 win32api.keybd_event(32,0,0,0)
-            print "looking"
         else:
-            if status != "not looking":
-                status = "not looking"
-                # press space to stop playing
-                win32api.keybd_event(32,0,0,0)
-            print "not looking"
+            if status is LOOKING:
+                if counter + 1 >= ENSURE_COUNT:
+                    status = NOT_LOOKING
+                    # press space to stop playing
+                    win32api.keybd_event(32,0,0,0)
+                    counter = 0
+                else:
+                    counter += 1
+        print status 
